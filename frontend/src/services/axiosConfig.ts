893e73fa6009a0833b995b6bf231332db0e0282a
Joonas Hiltunen <joonas@jontzi.com>
2022-02-20 12:59:39 +0200

    Add backend URL as env parameter

--- frontend/src/services/axiosConfig.ts
+++ frontend/src/services/axiosConfig.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const client = axios.create({
-    baseURL: 'https://omatpysakit.jontzi.com/api'
+    baseURL: process.env.REACT_APP_BACKEND_URL
 });
 
 export default client;
