91bf9541cb57569ca9abf5aed055d4cfe3b70e1c
Joonas Hiltunen <joonas@jontzi.com>
2022-03-05 00:54:26 +0200

    Add test todo

---
+++ frontend/src/components/supportScreens/header/header.test.tsx
@@ -0,0 +1,9 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import React from 'react';
+import { render, screen, waitFor } from '@testing-library/react';
+import AppHeader from './header';
+
+describe('AppHeader', () => {
+    test.todo('Should render text with button leading to frontpage');
+    test.todo('Should render button that opens menu');
+});
