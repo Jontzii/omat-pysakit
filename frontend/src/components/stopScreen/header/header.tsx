6abcae8b1bd1333a8a3573dadcb7197419c673c7
Joonas Hiltunen <joonas@jontzi.com>
2022-03-05 00:22:40 +0200

    Add loading spinner for stop screen

--- frontend/src/components/stopScreen/header/header.tsx
+++ frontend/src/components/stopScreen/header/header.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
 
-const ScreenHeader = () => {
+interface HeaderProps {
+    stopNames: string[];
+}
+
+const ScreenHeader = (props: HeaderProps) => {
     return (
-        <nav className="bg-nysse-blue-light border-solid border-b-2 border-clear-white"></nav>
+        <nav className="bg-nysse-blue-light border-solid border-b-2 border-clear-white">
+            YAAS
+        </nav>
     );
 };
 
--- frontend/src/components/stopScreen/stopScreen.tsx
+++ frontend/src/components/stopScreen/stopScreen.tsx
@@ -1,7 +1,39 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useSearchParams } from 'react-router-dom';
+
+import ScreenHeader from './header/header';
+import PageSpinner from '../uiElements/pageSpinner';
+
+import { getScreen } from '../../services/screenService';
 
 const StopScreen = () => {
-    return <div>STOP SCREEN</div>;
+    let [searchParams, setSearchParams] = useSearchParams();
+    const [stops, setStops] = useState<string[] | null>(null);
+    const [uuid, setUuid] = useState(searchParams.get('id'));
+    const [isLoading, setLoading] = useState(true);
+    const [notFound, setNotFound] = useState(false);
+
+    const fetchScreen = async () => {
+        if (!uuid) return null;
+
+        try {
+            const data = await getScreen(uuid);
+            setLoading(false);
+        } catch {
+            setLoading(false);
+            setNotFound(true);
+        }
+    };
+
+    useEffect(() => {
+        fetchScreen();
+    }, []);
+
+    return (
+        <div className="w-screen h-screen bg-nysse-blue-light">
+            <PageSpinner />
+        </div>
+    );
 };
 
 export default StopScreen;
